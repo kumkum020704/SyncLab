e1454243bb6a780671d02b584fcf944965bb4de8
Ms Kumkum Kadhala <22bcs165@iiitdmj.ac.in>
2026-03-24 20:01:57 +0530

    Initial commit: SyncLab base setup

---
+++ src/socket.js
@@ -0,0 +1,11 @@
+import { io } from 'socket.io-client';
+
+export const initSocket = async () => {
+    return io('http://localhost:5000', {
+        forceNew: true,
+        reconnection: true,
+        reconnectionAttempts: 5,
+        reconnectionDelay: 1000,
+        timeout: 20000,
+    });
+};
